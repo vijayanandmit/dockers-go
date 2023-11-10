d9938910a2220d298241dd26723d6268bd84d0c7
vijay <vijaycan@icloud.com>
2023-11-09 22:31:43 -0500

    fix error

--- app/main.go
+++ app/main.go
@@ -12,7 +12,7 @@ import (
 // Usage: your_docker.sh run <image> <command> <arg1> <arg2> ...
 func main() {
 	// You can use print statements as follows for debugging, they'll be visible when running tests.
-	fmt.Println("Logs from your program will appear here!")
+	//	fmt.Println("Logs from your program will appear here!")
 
 	// Uncomment this block to pass the first stage!
 	//
@@ -32,5 +32,5 @@ func main() {
 		os.Exit(1)
 	}
 
-	fmt.Println(string(output))
+	// fmt.Println(string(output))
 }
